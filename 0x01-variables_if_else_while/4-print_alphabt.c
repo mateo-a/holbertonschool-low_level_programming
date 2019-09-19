ac48c7e31f2339f6d420ae4044afcb47cc2b7906
mateo-a <alexander.urrego@gmail.com>
2019-09-19 20:27:51 +0000

    Add file 4-print...

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+	if (a != 113 && a != 101)
+	{
+		putchar(a);
+	}
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
