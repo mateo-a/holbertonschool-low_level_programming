4c9d9d48744f5ce0e39ba4652d8e15fa53ccd439
mateo-a <alexander.urrego@gmail.com>
2019-09-19 20:11:12 +0000

    Add file 3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	a = 65;
+	while (a <= 90)
+	{
+		putchar(a);
+		a++;
+	}
+return (0);
+}
