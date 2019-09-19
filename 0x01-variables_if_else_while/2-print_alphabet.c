6aa28841190677b003845171c5232028338f3d13
mateo-a <alexander.urrego@gmail.com>
2019-09-19 19:08:22 +0000

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i;
+	i = a;
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		putchar('\n');
+		i++;
+	}
+	return (0);
+}
