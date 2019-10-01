20aafb562cbc7760fe3f85bdba2a9b8f69be49e4
mateo-a <alexander.urrego@gmail.com>
2019-10-01 15:24:18 +0000

    Add 6-puts2.c Correction_2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,13 +13,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != 0)
+	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
+	if (i % 2 == 0)
+	{
+		_putchar(str[i]);
 	}
-	_putchar('\n');
+	i++;
+	}
+_putchar('\n');
 }
