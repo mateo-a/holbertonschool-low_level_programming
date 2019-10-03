f7b91a641ba708ba04ccee0a36e9d6ef856fa256
mateo-a <alexander.urrego@gmail.com>
2019-10-03 22:33:29 +0000

    Add file 8-rot13.c Correction_4

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,1 +1,22 @@
-#include
+#include "holberton.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ *
+ * @s: string
+ * Return: string
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'm')
+			s[i] = s[i] + 13;
+		else if (s[i] >= 'n' && s[i] <= 'z')
+			s[i] = s[i] - 13;
+	}
+	return (s);
+}
