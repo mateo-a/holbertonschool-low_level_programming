50b6b39bad07b3957fee72744f34443106f8fd09
mateo-a <alexander.urrego@gmail.com>
2019-10-03 22:04:29 +0000

    Add file 8-rot13.c Correction

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,19 +11,12 @@ char *rot13(char *s)
 {
 int i;
 
-i = 0;
-while (s[i] != '\0')
+for (i = 0; s[i] != '\0'; i++)
 {
-if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-{
-	s[i] = (s[i] + 12);
-}
-else
-while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-	{
-	s[i] = (s[i] - 13);
-	}
-	i++;
+	if (s[i] >= 'a' && s[i] <= 'm')
+		s[i] = s[i] + 13;
+	else if (s[i] >= 'n' && s[i] <= 'z')
+		s[i] = s[i] - 13;
 }
 return (s);
 }
