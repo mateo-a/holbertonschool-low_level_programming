ba79ab84475becc6c24523e7e0639e37ee51aaf7
mateo-a <alexander.urrego@gmail.com>
2019-10-03 23:00:20 +0000

    Add file 8-rot13.c Correction_5

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,22 +1,29 @@
 #include "holberton.h"
 
 /**
- * rot13 - function that encodes a string using rot13
+ * rot13 - converts string to rot13
  *
- * @s: string
- * Return: string
+ * @s: pointer to a string
+ * Return: pointer to char
  */
 
 char *rot13(char *s)
 {
-	int i;
+	int i, j;
+	char let[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 'a' && s[i] <= 'm')
-			s[i] = s[i] + 13;
-		else if (s[i] >= 'n' && s[i] <= 'z')
-			s[i] = s[i] - 13;
+	j = 0;
+	while ((let[j] != '\0') && (s[i] != let[j]))
+	{
+		j++;
 	}
-	return (s);
+	if (s[i] == let[j])
+	{
+		s[i] = rot[j];
+	}
+	}
+return (s);
 }
