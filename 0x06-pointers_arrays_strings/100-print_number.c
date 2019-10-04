acd0827750f63bdb6190085980bd2b7698f2cc42
mateo-a <alexander.urrego@gmail.com>
2019-10-04 00:30:50 +0000

    Add file 100-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_number - Function that prints an integer
+ *
+ * @n: integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int  sign, num;
+
+	num = 1000000000;
+	sign = 1;
+	if (n < 0)
+	{
+		n *= -1;
+		sign *= -1;
+		}
+	if  (n != 0)
+	{
+		while ((n / num) ==  0)
+		{
+			num = num / 10;
+		}
+		if (sign == -1)
+			_putchar('-');
+		while (num >= 1)
+		{
+			_putchar((n / num) + '0');
+			n = n % num;
+			num = num / 10;
+		}
+	}
+	else
+		_putchar('0');
+}
