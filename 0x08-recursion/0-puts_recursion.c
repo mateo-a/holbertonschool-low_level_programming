3bd60e72c80154c2727e0e58f0e5e1de497ea091
mateo-a <alexander.urrego@gmail.com>
2019-10-09 21:42:17 +0000

    File 0-puts_recursion Fix1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	int i = 0;
+
+	if (s[i] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
+	_putchar(s[i]);
 	s++;
 	_puts_recursion(s);
 }
