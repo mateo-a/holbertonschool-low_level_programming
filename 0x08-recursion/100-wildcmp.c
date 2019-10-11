bf25c844dfda528002998b5b68d693cc2f51d99e
mateo-a <alexander.urrego@gmail.com>
2019-10-11 02:25:37 +0000

    Add file 100-wildcmp.c Fix_2

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * wildcmp - function that compares two strings and returns 1 if the
- *           strings can be considered identical, otherwise return 0
+ * _puts_recursion - function that compares two strings and returns 1 if the
+ *                   strings can be considered identical, otherwise return 0
  *
  *
  * @s1: string
  * @s2: string
- * Return: void
+ * Return: 1 if strings identical and 0 otherwise
  */
 
 int wildcmp(char *s1, char *s2)
@@ -16,8 +16,8 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	if (*s2 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
 		return (0);
 	return (0);
