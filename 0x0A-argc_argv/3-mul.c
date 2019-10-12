034281bd814a3e0b51906e533d28b6156f8211b7
mateo-a <alexander.urrego@gmail.com>
2019-10-12 19:41:39 +0000

    Add file 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: arguement count
+ * @argv: pointer to array of strings arguments
+ * Return: none
+ */
+
+int main(int argc, char **argv)
+{
+	int mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+			   printf("%d\n", mul);
+			   }
+}
