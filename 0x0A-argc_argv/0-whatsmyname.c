97b9e4edbb6007f58799c2fdd64a42a20b8f2f27
mateo-a <alexander.urrego@gmail.com>
2019-10-12 18:44:23 +0000

    Fix add 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: arguement count
+ * @argv: pointer to array of strings arguments
+ * Return: none
+ */
+
 int main(__attribute__((unused)) int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
