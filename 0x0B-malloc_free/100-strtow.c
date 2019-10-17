b3cc93e7478d766efd971f4c586c07899bb93ffe
mateo-a <alexander.urrego@gmail.com>
2019-10-17 02:26:46 +0000

    Fix1 100-strtow.c

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -9,43 +9,31 @@
 
 char **strtow(char *str)
 {
-	char **array;
-	int i, j, count, len, k, m;
+	int i, k, j = 0, words = 1, len = 0;
+	char **p;
 
-	count = k = 0;
-	if (str == NULL || str[0] == '\0')
+	if (str == NULL || str == '\0')
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-			count++;
+		if (str[i] == 32 && str[i + 1] != 32)
+			words++;
 	}
-	if (count == 0)
-		return (NULL);
-	array = malloc(((count + 1) * sizeof(char *)));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; str[i] !=  '\0' && k < count; i++)
+	p = malloc(words * sizeof(char *));
+	for (i = 0; i < words; i++)
 	{
-		if (str[i] != ' ')
+		len = 0;
+		for (; str[j] != '\0'; j++)
 		{
-			len = 0;
-			j = i;
-			while (str[j] != ' ' && str[j] != '\0')
-				j++, len++;
-			array[k] = malloc((len + 1) * sizeof(char));
-			if (array[k] == NULL)
-			{
-				for (k = k - 1; k >= 0; k++)
-					free(array[k]);
-				free(array);
-				return (NULL);
-			}
-			for (m = 0; m < len; m++, i++)
-				array[k][m] = str[i];
-			array[k++][m] = '\0';
+			if (str[j] != 32)
+				len++;
+			else
+				break;
 		}
+		p[i] = malloc(1 + len * sizeof(char));
+		for (k = 0; k < len; k++)
+			p[i][k] = str[j - len + k];
+		p[i][len] = '\0';
 	}
-	array[k] = NULL;
-	return (array);
+	return (p);
 }
