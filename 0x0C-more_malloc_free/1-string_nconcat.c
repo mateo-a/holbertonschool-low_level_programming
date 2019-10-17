84611752ce7ad20e10e98ac9765c8cf0ee6f8502
mateo-a <alexander.urrego@gmail.com>
2019-10-17 17:19:07 +0000

    Fix3 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,23 +11,23 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *p, *emp;
-        unsigned int i, k;
+	char *p, *emp;
+	unsigned int i, k;
 
-        emp = "";
-        if (s1 == NULL)
-                s1 = emp;
-        if (s2 == NULL)
-                s2 = emp;
-        for (i = 0; s1[i] != '\0'; i++)
-                ;
-        p = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*p)));
-        if (p == NULL)
-                return (NULL);
-        for (i = 0; s1[i] != '\0'; i++)
-                p[i] = s1[i];
-        for (k = 0; s2[k] != '\0' && k < n; k++, i++)
-                p[i] = s2[k];
-        p[i] = '\0';
-        return (p);
+	emp = "";
+	if (s1 == NULL)
+		s1 = emp;
+	if (s2 == NULL)
+		s2 = emp;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	p = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*p)));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+	for (k = 0; s2[k] != '\0' && k < n; k++, i++)
+		p[i] = s2[k];
+	p[i] = '\0';
+	return (p);
 }
