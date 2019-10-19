06aa01c2e8cf950c7c539ea663d9a23d3f3667b8
mateo-a <alexander.urrego@gmail.com>
2019-10-19 04:41:48 +0000

    Fix4 add 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,31 @@
 #include "holberton.h"
+#include <string.h>
+
+/**
+ * main - program that multiplies two positive numbers
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+
+/**
+ * _isdigit - checks if it is fully a number
+ * @num: string to validate
+ * Return: 1 if is digit otherwise 0
+ */
+
+int _isdigit(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= '0' && c[i] <= '9')
+			return (1);
+	}
+	return (0);
+}
 
 /**
  * main - program that multiplies two positive numbers
@@ -10,16 +37,43 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, mul;
+	int l1, l2, len, i, j, mul, carry;
+	int *total;
 
-	if (argc != 3)
+	if (argc != 3 || !(_isdigit(argv[1])) || !(_isdigit(argv[2])))
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(98);
 	}
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-	mul = i * j;
-	printf("%d\n", mul);
-return (0);
+	l1 = strlen(argv[1]);
+	l2 = strlen(argv[2]);
+	len = l1 + l2;
+	total = calloc(len, sizeof(int *));
+	if (total == NULL)
+		puts("Error"), exit(98);
+	for (i = l2 - 1; i > -1; i--)
+	{
+		carry = 0;
+		for (j = l1 - 1; j > -1; j--)
+		{
+			mul = (argv[2][i] - '0') * (argv[1][j] - '0');
+			carry =  (mul / 10);
+			total[(i + j) + 1] += (mul % 10);
+			if (total[(i + j) + 1] > 9)
+			{
+				total[i + j] += total[(i + j) + 1] / 10;
+				total[(i + j) + 1] = total[(i + j) + 1] % 10;
+			}
+			total[(i + j)] += carry;
+		}
+	}
+	if (total[0] == 0)
+		i = 1;
+	else
+		i = 0;
+	for (; i < len; i++)
+		printf("%d", total[i]);
+	printf("\n");
+	free(total);
+	return (0);
 }
