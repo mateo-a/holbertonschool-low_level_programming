100eb38b0e87b979a24173647957fb6881b1b145
mateo-a <alexander.urrego@gmail.com>
2019-10-21 19:27:37 +0000

    Fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,4 @@ struct dog
 	char *owner;
 
 };
-/**
- * dog_t - typedef for dog
- */
-typedef struct dog dog_t;
-void free_dog(dog_t *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 #endif
