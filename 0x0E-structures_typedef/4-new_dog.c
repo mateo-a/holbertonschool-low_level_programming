602e8051d4db6a5fd9162945f61bd89d91c7c892
mateo-a <alexander.urrego@gmail.com>
2019-10-22 01:19:55 +0000

    Fix 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
-	new_dog->age = age;
+	if (new_dog->name == 0)
+	{
+		free(new_dog);
+		return (0);
+	}
 	new_dog->owner = owner;
+	if (new_dog == 0)
+	{
+		free(new_dog);
+		return (0);
+	}
+	new_dog->age = age;
 	return (new_dog);
 }
