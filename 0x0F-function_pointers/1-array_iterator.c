55b06bd5dc3138d47671e90dc3358797c6765ab8
mateo-a <alexander.urrego@gmail.com>
2019-10-24 00:43:36 +0000

    Fix betty - 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name
+ * array_iterator - function that executes a function given as a parameter
+ *                  on each element of an array
  * @array: array of elements
- * @size_t: size of the array
+ * @size: size of the array
  * @action: pointer to the function to be used
  * Return: nothing
  */
