16e09b0f97a78a01e06a6c3c1965205c52dbde4d
mateo-a <alexander.urrego@gmail.com>
2019-10-24 01:57:06 +0000

    Add files task #3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds;
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract;
+ * @a : integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies;
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - op div;
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod func
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
