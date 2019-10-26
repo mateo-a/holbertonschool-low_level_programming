1b857d86a819d458b1e112892b615c9e016d639e
mateo-a <alexander.urrego@gmail.com>
2019-10-26 17:31:07 +0000

    [FIX] file 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,12 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i, sum = 0;
 	va_list num;
-	int sum = 0;
-	unsigned int i;
 
-	if (n == 0)
-		return (0);
 	va_start(num, n);
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(num, int);
-	}
 	va_end(num);
 	return (sum);
 }
