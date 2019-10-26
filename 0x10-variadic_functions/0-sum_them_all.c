0e1ebe7cbbbc0cc8ad37a5ba10696dc5a704f0dd
mateo-a <alexander.urrego@gmail.com>
2019-10-26 18:02:08 +0000

    [FIX 3] Requirements for 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,27 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
+
 /**
- * sum_them_all - Sum of all its parameters
- * @n: Number of arguments
- *
- * Description: function that returns the
- * sum of all its parameters.
- * Return: The sum of all its parameters
+ * sum_them_all - sum all arguements
+ * @n: number of arguements
+ * Return: 0 is n is 0 or return sum
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int add = 0, i = 0;
-	va_list arguments;
+	va_list numbers;
+	int sum;
+	unsigned int i;
 
-	va_start(arguments, n);
-	while (i < n)
-		add += va_arg(arguments, int),  i++;
-	va_end(arguments);
-	return (add);
+	if (n == 0)
+		return (0);
+	sum = 0;
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (sum);
 }
