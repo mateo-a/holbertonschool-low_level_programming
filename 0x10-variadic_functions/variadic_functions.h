f66849d8670a1b0b02bb90c4d344c6597c8da2ad
mateo-a <alexander.urrego@gmail.com>
2019-10-26 18:34:50 +0000

    [FIX] file variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,13 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct fm - struct
+ * @fm: char
+ * @p: pointer
+ *
+ */
+
 typedef struct fm
 {
 	char *fm;
