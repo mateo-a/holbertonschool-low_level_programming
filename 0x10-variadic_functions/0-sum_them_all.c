10f553d36376f51049bef8af49ed866186c858c8
mateo-a <alexander.urrego@gmail.com>
2019-10-26 20:41:27 +0000

    [FIX 5] Betty Requirements in 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,12 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-        unsigned int i, sum = 0;
-        va_list num;
+	unsigned int i, sum = 0;
+	va_list num;
 
-        va_start(num, n);
-        for (i = 0; i < n; i++)
-                sum += va_arg(num, int);
-        va_end(num);
-        return (sum);
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(num, int);
+	va_end(num);
+	return (sum);
 }
