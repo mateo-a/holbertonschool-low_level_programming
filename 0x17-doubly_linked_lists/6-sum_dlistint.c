e803617a22775460bdc81a9fd8ca15ad0b1e5ab4
mateo-a <alexander.urrego@gmail.com>
2019-12-15 18:25:24 -0500

    [Fix] Fix Betty style

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,10 +11,10 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-        while (head != NULL)
-        {
+	while (head != NULL)
+	{
 		sum += head->n;
 		head = head->next;
-        }
-        return (sum);
+	}
+return (sum);
 }
