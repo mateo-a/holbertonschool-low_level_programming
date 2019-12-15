34d1cc432d4233a0d21f3b92beb7dbd1cdbefc07
mateo-a <alexander.urrego@gmail.com>
2019-12-15 17:51:15 -0500

    [Fix] Fixing betty style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - function that adds a new node at the beginning of a dlistint_t
- *              list
- * @h: pointer to head
+ * add_dnodeint - function that adds a new node at the beginning of
+ *                a dlistint_t list
+ * @head: double pointer to head
+ * @n: value to add
  * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
