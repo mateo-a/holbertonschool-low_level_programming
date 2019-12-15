3e3a78057c8c203ff54047ed08001eb9f7f6ba08
mateo-a <alexander.urrego@gmail.com>
2019-12-15 18:23:06 -0500

    [Fix] Fix Betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,19 +4,20 @@
  * get_dnodeint_at_index - function that returns the nth node of a dlistint_t
  *                         linked list
  * @head: head of the list
- * @index:the index of the node, starting from 0
+ * @index: the index of the node, starting from 0
+ * Return: the nth node or null if the node does not exist
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-        unsigned int count = 0;
+	unsigned int count = 0;
 
-        while (head != NULL && count <= index)
-        {
-                if (count == index)
-                        return (head);
-                head = head->next;
-                count++;
-        }
-        return (NULL);
+	while (head != NULL && count <= index)
+	{
+		if (count == index)
+			return (head);
+		head = head->next;
+		count++;
+	}
+	return (NULL);
 }
