36464759035f71157a634ff7e66f187f25271efd
mateo-a <alexander.urrego@gmail.com>
2020-02-21 23:11:24 -0500

    [Add] Tasks Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function that returns the perimeter of the island 
+described in grid
+"""
+
+
+def island_perimeter(grid):
+    """Find the perimeter"""
+    count = 0
+
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                count += 4
+                if x > 0 and grid[x - 1][y] == 1:
+                    count -= 2
+                if y > 0 and grid[x][y - 1] == 1:
+                    count -= 2
+    return count
