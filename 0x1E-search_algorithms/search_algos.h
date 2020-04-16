54bd4fef7cf108ded6f674032620d0dfdbb83dbe
mateo-a <alexander.urrego@gmail.com>
2020-04-15 20:07:36 -0500

    [ADD] Add Task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef search_algos_
+#define search_algos_
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
